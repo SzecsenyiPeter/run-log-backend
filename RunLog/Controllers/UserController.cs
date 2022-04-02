4fe464cf34344fbe2165fadc1209152f33a32d6c
Péter Szécsényi <petersz@topdesk.com>
2022-04-02 20:51:38 +0200

    Register kész

---
+++ RunLog/Controllers/UserController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RunLog.Data;
+using RunLog.Dto;
+using RunLog.Model;
+using System;
+
+namespace RunLog.Controllers
+{
+    [ApiController]
+    [Route("users")]
+    public class UserController : ControllerBase
+    {
+        private readonly RunLogContext runLogContext;
+
+        public UserController()
+        {
+            runLogContext = new RunLogContext();
+        }
+        [Route("register")]
+        [HttpPost]
+        public bool RegisterUser(CreateUser createUser)
+        {
+
+            User newUser = new User
+            {
+                Username = createUser.Username,
+                PasswordHash = HashString(createUser.Password),
+                UserType = createUser.UserType,
+
+            };
+            runLogContext.Users.Add(newUser);
+            runLogContext.SaveChanges();
+            return true;
+        }
+        static string HashString(string text, string salt = "")
+        {
+            if (String.IsNullOrEmpty(text))
+            {
+                return String.Empty;
+            }
+
+            using (var sha = new System.Security.Cryptography.SHA256Managed())
+            {
+                byte[] textBytes = System.Text.Encoding.UTF8.GetBytes(text + salt);
+                byte[] hashBytes = sha.ComputeHash(textBytes);
+
+                string hash = BitConverter
+                    .ToString(hashBytes)
+                    .Replace("-", String.Empty);
+
+                return hash;
+            }
+        }
+    }
+}
---
+++ RunLog/Dto/CreateUser.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using RunLog.Model;
+
+namespace RunLog.Dto
+{
+    public class CreateUser
+    {
+        [JsonPropertyName("username")]
+        [Required]
+        public string Username { get; set; }
+        [JsonPropertyName("password")]
+        [Required]
+        public string Password { get; set; }
+
+        [JsonPropertyName("userType")]
+        [Required]
+        public UserTypes UserType { get; set; }
+
+    }
+}
